21f76dbe1ba51968db3dcb613234a6e07766b16f
bongjooncha <cbj4133@naver.com>
2025-10-02 03:30:07 +0900

    initial commit

---
+++ SpacialMoodBoard/SpacialMoodBoardApp.swift
@@ -0,0 +1,33 @@
+//
+//  SpacialMoodBoardApp.swift
+//  SpacialMoodBoard
+//
+//  Created by apple on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SpacialMoodBoardApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
