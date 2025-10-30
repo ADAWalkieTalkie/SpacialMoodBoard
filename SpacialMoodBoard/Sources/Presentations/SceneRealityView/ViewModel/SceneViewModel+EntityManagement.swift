4c26f539bb721c15d6e83a5034d39ec7689c0f5d
Penguin <149217055+Potalzz@users.noreply.github.com>
2025-10-31 05:34:39 +0900

    refactor: RoomManagement 삭제 후 내부 메서드 EntityManagement로 이동

--- SpacialMoodBoard/Sources/Presentations/SceneRealityView/ViewModel/SceneViewModel+EntityManagement.swift
+++ SpacialMoodBoard/Sources/Presentations/SceneRealityView/ViewModel/SceneViewModel+EntityManagement.swift
@@ -31,9 +31,26 @@ extension SceneViewModel {
             rootEntity: rootEntity
         )
     }
+    
+    /// 특정 프로젝트의 엔티티 캐시 삭제
+    func deleteEntityCache(for project: Project) {
+        entityRepository.clearFloorCache()
+
+        if appModel.selectedProject?.id == project.id {
+            appModel.selectedProject = nil
+        }
+    }
 
     /// 특정 ID의 엔티티를 가져오기
     func getEntity(for id: UUID) -> ModelEntity? {
         return entityRepository.getEntity(for: id)
     }
+    
+    /// Floor 엔티티를 가져오거나 생성
+    /// EntityRepository에 작업을 위임
+    func getFloorEntity() -> ModelEntity? {
+        let environment = self.spacialEnvironment
+        return entityRepository.getOrCreateFloorEntity(from: environment)
+    }
+
 }
