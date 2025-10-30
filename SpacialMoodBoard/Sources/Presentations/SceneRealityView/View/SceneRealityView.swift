4a2ca8accd1e8266a7e187be223ec557edfda6f2
bongjooncha <cbj4133@naver.com>
2025-10-31 03:11:39 +0900

    remove: Extension 파일 이동

--- SpacialMoodBoard/Sources/Presentations/SceneRealityView/View/SceneRealityView.swift
+++ SpacialMoodBoard/Sources/Presentations/SceneRealityView/View/SceneRealityView.swift
@@ -290,16 +290,3 @@ struct SceneRealityView: View {
         }
     }
 }
-
-// MARK: - View Extension
-
-extension View {
-    @ViewBuilder
-    func `if`<Transform: View>(_ condition: Bool, transform: (Self) -> Transform) -> some View {
-        if condition {
-            transform(self)
-        } else {
-            self
-        }
-    }
-}
