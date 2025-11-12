83930f0fd854949d53cd513e48f4ee15f9626ea6
bongjooncha <cbj4133@naver.com>
2025-11-13 03:31:17 +0900

    remove: FloorEntity에 human생성 로직 제거

--- Glayer/Sources/Helpers/Entity/Environment/FloorEntity.swift
+++ Glayer/Sources/Helpers/Entity/Environment/FloorEntity.swift
@@ -108,44 +108,4 @@ class FloorEntity {
 
         return material
     }
-    
-    // MARK: - Human Scale Entity 생성
-
-    /// 휴먼 스케일 가이드 Entity를 생성합니다
-    /// - Parameter size: 스케일 가이드 크기
-    /// - Returns: HumanScaleGuide10 이미지를 사용한 ModelEntity
-    @MainActor
-    static func createHumanScaleObject(size: SIMD2<Float>) async -> ModelEntity {
-        var material = PhysicallyBasedMaterial()
-
-        do {
-            if let uiImage = UIImage(named: "img_humanScaleGuide"),
-               let cgImage = uiImage.cgImage {
-                let texture = try await TextureResource(image: cgImage, options: .init(semantic: .color))
-
-                material.baseColor = .init(texture: .init(texture))
-                material.emissiveColor = .init(texture: .init(texture))
-                material.emissiveIntensity = 10.0
-                material.metallic = .init(floatLiteral: 0.0)
-                material.roughness = .init(floatLiteral: 0.8)
-                material.faceCulling = .none
-                material.blending = .transparent(opacity: .init(texture: .init(texture)))
-                material.opacityThreshold = 0.3
-            } else {
-                print("HumanScale Load 실패: Asset Catalog에서 이미지를 찾을 수 없습니다")
-            }
-        } catch {
-            print("HumanScale Load 실패: \(error)")
-            print(error.localizedDescription)
-        }
-        
-        let humanScaleEntity = ModelEntity(
-                mesh: .generatePlane(width: size.x, height: size.y),
-                materials: [material]
-            )
-
-        humanScaleEntity.position = [0, (size.y / 2.55) , 0]
-
-        return humanScaleEntity
-    }
 }
