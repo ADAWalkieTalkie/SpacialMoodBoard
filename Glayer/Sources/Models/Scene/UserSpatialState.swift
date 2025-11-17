591cd079e9cc1f44330f6d18cda55822fc6fdf6f
bongjooncha <cbj4133@naver.com>
2025-11-17 19:22:31 +0900

    feat: sceneHeadAnchor을 추가해 userPosition, headAnchorState 합친 값 생성. 불필요한 updateUserPosition제거

--- Glayer/Sources/Models/Scene/UserSpatialState.swift
+++ Glayer/Sources/Models/Scene/UserSpatialState.swift
@@ -16,10 +16,23 @@ struct UserSpatialState: Codable, Hashable {
         self.viewMode = viewMode
         self.paused = paused
     }
+    
+    /// headAnchor 위치와 조이스틱 이동을 합친 실제 유저 위치 및 회전
+    var sceneHeadAnchor: HeadAnchorState {
+        return HeadAnchorState(
+            position: headAnchorState.position + userPosition,
+            rotation: headAnchorState.rotation
+        )
+    }
 }
 
 // MARK: - HeadAnchorState (HeadAnchor의 위치와 회전 정보)
 struct HeadAnchorState: Codable, Hashable {
     var position: SIMD3<Float> = [0, 0, 0]
     var rotation: SIMD4<Float> = [0, 0, 0, 1]  // quaternion as SIMD4
+    
+    init(position: SIMD3<Float> = [0, 0, 0], rotation: SIMD4<Float> = [0, 0, 0, 1]) {
+        self.position = position
+        self.rotation = rotation
+    }
 }
