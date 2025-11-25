be08554d6bc7a8e7e5b63b6540a0d5f50eb354ea
bongjooncha <cbj4133@naver.com>
2025-11-25 23:07:25 +0900

    fix: project_json 파일 변경

--- Glayer/Sources/Helpers/FileManager/Core/FilePathProvider.swift
+++ Glayer/Sources/Helpers/FileManager/Core/FilePathProvider.swift
@@ -26,7 +26,7 @@ struct FilePathProvider {
     // SceneModel 저장용 - Project 객체는 인메모리 DB에 저장
     static func projectMetadataFile(projectName: String) -> URL {
         projectDirectory(projectName: projectName)
-            .appendingPathComponent("\(projectName)_project.json")
+            .appendingPathComponent("project.json")
     }
     
     // MARK: - 프로젝트의 이미지
