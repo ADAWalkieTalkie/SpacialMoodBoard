02313486db29991d2346bdc0a15e020f7719bdb7
Penguin <149217055+Potalzz@users.noreply.github.com>
2025-12-31 12:08:00 +0900

    fix: LockIcon과 ImageEntity 간격 최소화

--- Glayer/Sources/Helpers/Entity/Attachment/AttachmentPositioner.swift
+++ Glayer/Sources/Helpers/Entity/Attachment/AttachmentPositioner.swift
@@ -59,7 +59,7 @@ enum AttachmentPositioner {
     ///   - parent: Attachment가 첨부될 부모 Entity
     static func positionAtMiddle(_ attachment: Entity, relativeTo parent: Entity) {
         // 중앙 위치는 parent의 중심점
-        attachment.position = SIMD3<Float>(0, 0, 0.01)
+        attachment.position = SIMD3<Float>(0, 0, 0)
     }
     
     /// 하단 위치로 Attachment 설정
