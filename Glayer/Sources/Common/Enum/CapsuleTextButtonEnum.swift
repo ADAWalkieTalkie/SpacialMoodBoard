82561f9f74a0a1aacb5a4555e170b5b173a73187
Penguin <149217055+Potalzz@users.noreply.github.com>
2026-01-05 19:08:53 +0900

    feat: 크롭 진행 중 버튼 UI 구현

--- Glayer/Sources/Common/Enum/CapsuleTextButtonEnum.swift
+++ Glayer/Sources/Common/Enum/CapsuleTextButtonEnum.swift
@@ -10,49 +10,63 @@ import SwiftUI
 enum CapsuleTextButtonEnum {
     case imageEditorView
     case dropDockOverlayView
-    
+    case cropCancel
+    case cropComplete
+
     var font: Font {
         switch self {
         case .imageEditorView:
             return .system(size: 17, weight: .medium)
         case .dropDockOverlayView:
             return .system(size: 19, weight: .medium)
+        case .cropCancel, .cropComplete:
+            return .system(size: 17, weight: .medium)
         }
     }
-    
+
     var fontColor: Color {
         switch self {
         case .imageEditorView:
             return .primary
         case .dropDockOverlayView:
             return .primary
+        case .cropCancel:
+            return .red
+        case .cropComplete:
+            return .white
         }
     }
-    
+
     var verticalPadding: CGFloat {
         switch self {
         case .imageEditorView:
             return 11
         case .dropDockOverlayView:
             return 6.5
+        case .cropCancel, .cropComplete:
+            return 17.5
         }
     }
-    
+
     var horizontalPadding: CGFloat {
         switch self {
         case .imageEditorView:
             return 16
         case .dropDockOverlayView:
             return 26
+        case .cropCancel, .cropComplete:
+            return 24
         }
     }
-    
+
     var background: AnyShapeStyle {
         switch self {
         case .imageEditorView:
             return AnyShapeStyle(.ultraThinMaterial)
         case .dropDockOverlayView:
             return AnyShapeStyle(.black.opacity(0.26))
+        case .cropCancel, .cropComplete:
+            return AnyShapeStyle(.ultraThinMaterial)
         }
     }
 }
---
+++ Glayer/Sources/Presentations/SceneRealityView/View/Attachment/Crop/CropControlAttachment.swift
@@ -0,0 +1,47 @@
+//
+//  CropControlAttachment.swift
+//  Glayer
+//
+//  Created by PenguinLand on 01/05/26.
+//
+
+import SwiftUI
+
+/// 이미지 크롭 중에 표시되는 취소/완료 버튼 UI
+/// CropAttachment 상단에 배치되어 사용자가 명시적으로 크롭을 완료하거나 취소할 수 있도록 함
+struct CropControlAttachment: View {
+
+    // MARK: - Properties
+
+    let onCancel: () -> Void
+    let onComplete: () -> Void
+
+    // MARK: - Body
+
+    var body: some View {
+        HStack(spacing: 20) {
+            // 취소 버튼
+            CapsuleTextButton(
+                title: "취소",
+                type: .cropCancel,
+                action: onCancel
+            )
+            .glassBackgroundEffect()
+
+            // 완료 버튼
+            CapsuleTextButton(
+                title: "완료",
+                type: .cropComplete,
+                action: onComplete
+            )
+            .glassBackgroundEffect()
+        }
+    }
+}
+
+#Preview {
+    CropControlAttachment(
+        onCancel: { print("취소") },
+        onComplete: { print("완료") }
+    )
+}
